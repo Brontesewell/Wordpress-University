b88ae4569a6a6213de2b78d4627f571ceea231f5
Brontesewell <brontsewell7@gmail.com>
2020-07-06 17:56:21 -0700

    search

---
+++ app/public/wp-content/themes/fictional university theme/js/modules/Search.js
@@ -0,0 +1,22 @@
+class Search {
+
+    // 1. Describle and create/initiate our obj
+    constructor(){
+        this.openButton = "";
+    }
+
+    // 2. events
+
+
+
+    // 3. methods (funcs, actions)
+    openOverlay(){
+
+    }
+
+    closeOverlay(){
+
+    }
+}
+
+export default Search;

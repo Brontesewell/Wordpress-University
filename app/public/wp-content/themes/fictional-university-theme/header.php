bc83dd250b5c8fd76830aa8badff7fa0ece340d5
Brontesewell <brontsewell7@gmail.com>
2020-07-23 07:44:53 -0700

    campus titles

--- app/public/wp-content/themes/fictional-university-theme/header.php
+++ app/public/wp-content/themes/fictional-university-theme/header.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php wp_head(); ?>
   </head>
+  
   <body <?php body_class(); ?>>
     <header class="site-header">
     <div class="container">
@@ -20,7 +21,7 @@
             <li <?php if (get_post_type() == 'campus') echo 'class="current-menu-item"' ?>><a href="<?php echo get_post_type_archive_link('campus'); ?>">Campuses</a></li>
             <li <?php if (get_post_type() == 'post') echo 'class="current-menu-item"' ?>><a href="<?php echo site_url('/blog'); ?>">Blog</a></li>
           </ul>
-          
+
         </nav>
         <div class="site-header__util">
           <a href="#" class="btn btn--small btn--orange float-left push-right">Login</a>
